9b2e4365bb7ae3a2d6c7d0e4163beb8f86618bde
Mohammed <study.computing2016@gmail.com>
2018-09-09 17:24:49 +0100

    This is BasketBall Score Tracking App

---
+++ BasketBallScoreApp/app/src/main/java/com/example/mohammed/basketballscoreapp/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.mohammed.basketballscoreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Gloabal variable to hold score
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * THis method will increase 3 points for team a
+     * and calling displayForTeamA method
+     * @param view
+     */
+    public void addThreeForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * THis method will increase 2 points for team a
+     * and calling displayForTeamA method
+     * @param view
+     */
+    public void addTwoForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * THis method will increase 1 points for team a
+     * and calling displayForTeamA method
+     * @param view
+     */
+    public void addOneForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * This displayForTeamA method will display the score for the team
+     * @param score
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * THis method will increase 3 points for team b
+     * and calling displayForTeamB method
+     * @param view
+     */
+    public void addThreeForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * THis method will increase 2 points for team b
+     * and calling displayForTeamB method
+     * @param view
+     */
+    public void addTwoForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * THis method will increase 1 points for team b
+     * and calling displayForTeamB method
+     * @param view
+     */
+    public void addOneForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This displayForTeamB method will display the score for the team
+     * @param score
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Score reset method
+     * first score will set 0 for both team
+     * second calling display method
+     * @param view
+     */
+    public void resetScore(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+}
